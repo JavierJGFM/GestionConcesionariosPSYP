88266df82758ca3c79c84adf15254ded9661b528
JavierJGFM <javiervivaes@gmail.com>
2016-12-19 20:49:40 +0100

    VF

--- createUser.sql
+++ createUser.sql
@@ -1,4 +1,3 @@
-CREATE USER empleado@localhost IDENTIFIED BY 'empleado';
-grant insert,update,delete,select on proyecto.Concesionario to empleado@localhost;
-grant insert,update,delete,select on proyecto.Vehiculo to empleado@localhost;
-grant execute on function proyecto.obtener_media to empleado@localhost;
+CREATE USER empleado IDENTIFIED BY 'empleado';
+grant insert,update,delete,select on proyecto.Concesionario to empleado;
+grant insert,update,delete,select on proyecto.Vehiculo to empleado;
